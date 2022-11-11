5ab82eda81404264d29d3131bbb9ae6d31757b95
Manish Sharma <sharmamanish16111999@gmail.com>
2022-11-11 21:10:21 +0530

    merge sort

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class Main
+{   
+    private void print(int[] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        return;
+    }
+
+    public static void main(String args[])
+    {   
+        int size = 6;
+        int[] arr = new int[size];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < size; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        Sort obj = new Sort(arr, 0, (arr.length) - 1);
+        obj.start();
+        try
+        {
+            obj.join();
+        }
+        catch(Exception e)
+        {
+
+        }
+        print(arr);
+        return;
+    }
+}
